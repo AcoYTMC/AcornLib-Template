aee3cf2da52fa23836a9b7ccbf0d94d75bd2e8d8
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-06-12 20:08:22 -0700

    Initial commit

---
+++ src/main/java/com/example/template/data/TemplateDatagen.java
@@ -0,0 +1,10 @@
+package com.example.template.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TemplateDatagen implements DataGeneratorEntrypoint {
+	public void onInitializeDataGenerator(FabricDataGenerator generator) {
+		//
+	}
+}
